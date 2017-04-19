3581015f6f265ba5cb477a5a8083fbcf49da4f3c
jorgeluisrocha <jlr435@cornell.edu>
2017-04-18 22:28:40 -0400

    prototype works for the data set

--- project_template/test.py
+++ project_template/test.py
@@ -2,23 +2,45 @@ from .models import Docs
 import os
 import Levenshtein
 import json
+import pickle
 
 
-def read_file(n):
-	path = Docs.objects.get(id = n).address;
-	file = open(path)
-	transcripts = json.load(file)
-	return transcripts
+def read_json(n):
+	file = open(n)
+	j = json.load(file)
+	return j
 
-def _edit(query, msg):
-    return Levenshtein.distance(query.lower(), msg.lower())
+def read_pickle(n):
+	p = pickle.load(open(n, 'rb'))
+	return p
+
+def query(q):
+	return q.lower()
 
 def find_similar(q):
-	transcripts = read_file(1)
-	result = []
-	for transcript in transcripts:
-		for item in transcript:
-			m = item['text']
-			result.append(((_edit(q, m)), m))
+	sims_m = read_json('backend_code/book_sims.json')
+	title_to_idx = read_pickle('backend_code/book_title_to_index.pickle')
+	idx_to_title = read_pickle('backend_code/book_index_to_title.pickle')
 
-	return sorted(result, key=lambda tup: tup[0])
+	lower_tti = {}
+
+	for k, v in title_to_idx.items():
+		k = k.lower()
+		lower_tti[k] = v
+
+	q = query(q)
+	q_idx = lower_tti[q]
+
+	sims_q = sims_m[q_idx]
+
+	result = []
+
+	for idx in range(len(sims_q)):
+		if (idx==145):
+			continue
+		else:
+			sim = sims_q[idx]
+			title = idx_to_title[idx]
+			result.append((sim, title))
+
+	return sorted(result, key=lambda tup: tup[0], reverse=True)
